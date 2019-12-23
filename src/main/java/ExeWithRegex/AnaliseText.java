635a6623ee2dbde4838f186f87441ae1ad90f8c1
jurekd01 <djurek@saba.com>
2019-12-23 07:35:30 +0100

    Exe with regex for analize and convert file

---
+++ src/main/java/ExeWithRegex/AnaliseText.java
@@ -0,0 +1,37 @@
+package ExeWithRegex;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnaliseText {
+    public static void main(String[] args) {
+        String txt = "1/2 /3/ 4";
+        String delim = "[ /]";
+
+        Scanner scan = new Scanner(txt).useDelimiter(delim);
+        System.out.print("Text : " + quote(txt) + "Separate: " + quote(delim));
+
+        while (scan.hasNext())
+            System.out.print(quote(scan.next()) + " ");
+
+        delim = "[ /]+";
+
+        scan = new Scanner(txt).useDelimiter(delim);
+        System.out.print("\nText : " + quote(txt) + " Separate: " + quote(delim));
+
+        while (scan.hasNext())
+            System.out.print(quote(scan.next()) + " ");
+
+        txt = " 1/2 /3/ 4";
+        scan = new Scanner(txt).useDelimiter(delim);
+        System.out.print("\nText : " + quote(txt) + " Separate: " + quote(delim));
+
+        while (scan.hasNext())
+            System.out.print(quote(scan.next()) + " ");
+        System.out.print("\nAnd split make:\n" + Arrays.toString(txt.split(delim)));
+    }
+
+    public static String quote(String s){
+        return "'" + s + "'" + " ";
+    }
+}
---
+++ src/main/java/ExeWithRegex/CountNumberFromText.java
@@ -0,0 +1,19 @@
+package ExeWithRegex;
+
+import java.util.Scanner;
+
+public class CountNumberFromText {
+    public static void main(String[] args) {
+        String txt = "Lorem Ipsum is simply dummy text of the 15 printing and typesetting industry.\n" +
+                " Lorem Ipsum has been the industry's standard 20 dummy text ever since the 1500, when\n" +
+                " an unknown printer took a galley of type and 10 scrambled it to make a type specimen book.";
+        String delim = "[^0-9]+";
+
+        Scanner scan = new Scanner(txt).useDelimiter(delim);
+        int sum = 0;
+        while (scan.hasNextInt()){
+            sum += scan.nextInt();
+        }
+        System.out.println("Text:\n" + txt + "\n\nCount number: " + sum);
+    }
+}
