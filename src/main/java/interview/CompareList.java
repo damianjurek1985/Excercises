0c4694e566d8a3cf4f96d3b171d0fc2412f862c6
jurekd01 <djurek@saba.com>
2020-02-28 21:23:10 +0100

    add new exe

--- src/main/java/interview/CompareList.java
+++ src/main/java/interview/CompareList.java
@@ -1,17 +1,37 @@
 package interview;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CompareList {
 
 
     public static void main(String[] args) {
-        compareByCollection();
+        findTheMostRepeatString();
     }
 
     public static List<String> one = new ArrayList<>(Arrays.asList("milan", "dingo", "elpha", "hafil", "meat", "iga", "neeta.peeta"));
     public static List<String> two = new ArrayList<>(Arrays.asList("hafil", "iga", "binga", "mike", "dingo"));
 
+    public static List<String> tree = new ArrayList<>(Arrays.asList("hafil", "iga", "binga", "mike", "dingo", "hafil","hafil","binga", "binga", "binga", "mike"));
+
+    public static void findTheMostRepeatString(){
+        Map<String, Long> countElementByName = tree.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        String maxRepeatValue = Collections.max(countElementByName.entrySet(), Comparator.comparingLong(Map.Entry::getValue)).getKey();
+        System.out.println(maxRepeatValue);
+        System.out.println(countElementByName);
+
+        System.out.println( tree.stream()
+                .collect(Collectors.toMap(Function.identity(), e -> 1, Math::addExact)));
+
+        Map<String, Integer> accumulator = new HashMap<>();
+        tree.forEach(s -> accumulator.merge(s, 1, Math::addExact));
+
+        System.out.println(accumulator);
+    }
 
     public static void compareByCollection(){
         Collection<String> similar = new HashSet<>( one );
@@ -23,7 +43,7 @@ public class CompareList {
         different.removeAll( similar );
         System.out.printf("One:%s%nTwo:%s%nSimilar:%s%nDifferent:%s%n", one, two, similar, different);
 
-        two.removeIf(one::contains);
+        //two.removeIf(one::contains);
         //return initial - two.size();
     }
 }
